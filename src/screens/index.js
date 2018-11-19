2f5ecdb6492475e1626a8c8c2c42a73d1ecbfc13
neptech <neptech4u@gmail.com>
2018-11-19 11:12:37 +0545

    initial configuration and setup done

---
+++ src/screens/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class index extends Component {
+
+  state={};
+
+  render() {
+    return (
+      <View>
+        <Text>Main Screen</Text>
+      </View>
+    )
+  }
+}
+
+export default index;
